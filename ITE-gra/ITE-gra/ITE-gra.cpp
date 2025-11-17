fcad7c9419b92415157e5a1b7b823966d9239125
AdamPodymniak <agpodymniak@gmail.com>
2025-11-17 20:49:02 +0100

    Podstawowy Movement

---
+++ ITE-gra/ITE-gra/ITE-gra.cpp
@@ -0,0 +1,152 @@
+﻿#include "raylib.h"
+#include "raymath.h"
+#include <cmath>
+
+struct Ghost {
+    Vector2 pos;
+    float angle;
+    float alpha;
+};
+
+int main() {
+    InitWindow(800, 600, "ITE-gra");
+    SetTargetFPS(60);
+
+    Vector2 position = { 400, 300 };
+
+    const float SIZE = 20.0f;
+    const float DASH_DISTANCE = 250.0f;
+    const float DASH_DELAY = 0.13f;
+    const float DASH_TIME = 0.13f;
+    const float GHOST_LIFETIME = 0.35f;
+    const int MAX_GHOSTS = 200;
+    const float GHOST_SPACING = 8.0f;
+
+    Ghost ghosts[MAX_GHOSTS] = {};
+    int ghostIndex = 0;
+
+    bool dashPending = false;
+    float dashTimer = 0.0f;
+
+    bool dashing = false;
+    float dashT = 0.0f;
+
+    Vector2 dashStart = { 0,0 };
+    Vector2 dashEnd = { 0,0 };
+    Vector2 dashDir = { 0,0 };
+    float dashAngle = 0;
+
+    float shakeTime = 0.0f;
+    float shakeStrength = 1.0f;
+
+    float accumulatedDistance = 0.0f;
+    Vector2 lastGhostPos = { 0,0 };
+
+    while (!WindowShouldClose()) {
+        float dt = GetFrameTime();
+        Vector2 mouse = GetMousePosition();
+        float angle = atan2f(mouse.y - position.y, mouse.x - position.x) * RAD2DEG;
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && !dashPending && !dashing) {
+            dashPending = true;
+            dashTimer = DASH_DELAY;
+
+            Vector2 dir = Vector2Subtract(mouse, position);
+            if (Vector2Length(dir) > 0)
+                dashDir = Vector2Normalize(dir);
+
+            dashStart = position;
+            dashEnd = {
+                position.x + dashDir.x * DASH_DISTANCE,
+                position.y + dashDir.y * DASH_DISTANCE
+            };
+
+            dashAngle = angle;
+            lastGhostPos = position;
+            accumulatedDistance = 0.0f;
+        }
+
+        if (dashPending) {
+            dashTimer -= dt;
+            if (dashTimer <= 0) {
+                dashPending = false;
+                dashing = true;
+                dashT = 0.0f;
+            }
+        }
+
+        if (dashing) {
+            dashT += dt / DASH_TIME;
+            if (dashT > 1.0f) dashT = 1.0f;
+
+            Vector2 prevPos = position;
+            position = Vector2Lerp(dashStart, dashEnd, dashT);
+
+            accumulatedDistance += Vector2Distance(prevPos, position);
+            while (accumulatedDistance >= GHOST_SPACING) {
+                Vector2 spawnPos = Vector2Lerp(lastGhostPos, position, GHOST_SPACING / accumulatedDistance);
+
+                Ghost g;
+                g.pos = spawnPos;
+                g.angle = dashAngle;
+                g.alpha = 1.0f;
+                ghosts[ghostIndex] = g;
+                ghostIndex = (ghostIndex + 1) % MAX_GHOSTS;
+
+                accumulatedDistance -= GHOST_SPACING;
+                lastGhostPos = spawnPos;
+            }
+
+            if (dashT >= 1.0f) {
+                dashing = false;
+                shakeTime = 0.12f;
+            }
+        }
+
+        for (int i = 0; i < MAX_GHOSTS; i++) {
+            if (ghosts[i].alpha > 0.0f)
+                ghosts[i].alpha -= dt / GHOST_LIFETIME;
+            if (ghosts[i].alpha < 0.0f)
+                ghosts[i].alpha = 0.0f;
+        }
+
+        Vector2 shakeOffset = { 0, 0 };
+        if (shakeTime > 0) {
+            shakeTime -= dt;
+            shakeOffset.x = GetRandomValue(-shakeStrength, shakeStrength);
+            shakeOffset.y = GetRandomValue(-shakeStrength, shakeStrength);
+        }
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        BeginBlendMode(BLEND_ALPHA);
+        for (int i = 0; i < MAX_GHOSTS; i++) {
+            if (ghosts[i].alpha > 0.0f) {
+                Rectangle r = {
+                    ghosts[i].pos.x - SIZE / 2 + shakeOffset.x,
+                    ghosts[i].pos.y - SIZE / 2 + shakeOffset.y,
+                    SIZE,
+                    SIZE
+                };
+                Color c = ColorAlpha(BLUE, ghosts[i].alpha * 0.5f);
+                DrawRectanglePro(r, { SIZE / 2, SIZE / 2 }, ghosts[i].angle, c);
+            }
+        }
+        EndBlendMode();
+
+        Rectangle r = {
+            position.x - SIZE / 2 + shakeOffset.x,
+            position.y - SIZE / 2 + shakeOffset.y,
+            SIZE,
+            SIZE
+        };
+        DrawRectanglePro(r, { SIZE / 2, SIZE / 2 }, angle, BLUE);
+
+        DrawText("LPM - DASH", 10, 10, 20, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
